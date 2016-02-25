cffe6b9457180176ff8e6da08ee90432b6e6c8bd
Jared Kerim <jared.l.kerim@gmail.com>
2016-02-25 17:35:55 +0000

    Strip protocol and query params from cache keys #18

--- embedly/tests.py
+++ embedly/tests.py
@@ -133,7 +133,7 @@ class TestEmbedlyProxy(unittest.TestCase):
 
         def get_mocked_cache_lookup(url, cached_data):
             def mocked_lookup(key):
-                if key == url:
+                if key == embedly.get_cache_key(url):
                     return json.dumps(cached_data)
 
             return mocked_lookup
@@ -181,6 +181,42 @@ class TestEmbedlyProxy(unittest.TestCase):
         response_data = json.loads(response.data)
 
         self.assertEqual(response_data, self.expected_response)
+
+    def test_protocol_and_query_are_removed_from_cache_key(self):
+
+        def mocked_lookup(url):
+            return json.dumps(self._get_url_data(url))
+
+        self.mock_redis.get.side_effect = mocked_lookup
+
+        similar_urls = [
+            'https://www.google.ca/?q=hello',
+            'http://www.google.ca/?q=hello',
+            'http://www.google.ca/?q=goodbye',
+            'http://www.google.ca/some/path/?q=derp',
+            'http://www.google.ca/some/path/?q=beep',
+        ]
+
+        response = self.client.get(self._build_query_url(similar_urls))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(self.mock_redis.get.call_count, 5)
+        self.assertEqual(self.mock_requests_get.call_count, 0)
+        self.assertEqual(len(self.mock_redis.get.call_args[0]), 1)
+        self.assertEqual(
+            set([call[0][0] for call in self.mock_redis.get.call_args_list]),
+            set(['www.google.ca/', 'www.google.ca/some/path/']),
+        )
+
+        response_data = json.loads(response.data)
+
+        expected_response = {
+            url: self._get_url_data(embedly.get_cache_key(url))
+            for url in similar_urls
+        }
+
+        self.assertEqual(response_data, expected_response)
 
 
 if __name__ == '__main__':
