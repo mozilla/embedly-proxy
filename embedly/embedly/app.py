50d251e3bc2301c0ca533d2e978cee044212ac8f
Jared Kerim <jared.l.kerim@gmail.com>
2016-05-18 18:18:22 +0000

    Split create_app into multiple sections #84

--- embedly/embedly/app.py
+++ embedly/embedly/app.py
@@ -9,27 +9,48 @@ import api.views
 from extract import URLExtractor
 
 
+def get_config():
+    return {
+        'MAXIMUM_POST_URLS': 25,
+        'EMBEDLY_URL': 'https://api.embedly.com/1/extract',
+        'EMBEDLY_KEY': os.environ.get('EMBEDLY_KEY', None),
+        'REDIS_TIMEOUT': 24 * 60 * 60,  # 24 hour timeout
+        'REDIS_URL': os.environ.get('REDIS_URL', None),
+        'SENTRY_DSN': os.environ.get('SENTRY_DSN', ''),
+        'SENTRY_PROCESSORS': ('raven.processors.RemovePostDataProcessor',),
+        'BLOCKED_DOMAINS': ['embedly.com'],
+    }
+
+
+def get_redis_client():  # pragma: nocover
+    config = get_config()
+
+    return redis.StrictRedis(host=config['REDIS_URL'], port=6379, db=0)
+
+
+def get_extractor(redis_client=None):
+    config = get_config()
+
+    return URLExtractor(
+        config['EMBEDLY_URL'],
+        config['EMBEDLY_KEY'],
+        redis_client or get_redis_client(),
+        config['REDIS_TIMEOUT'],
+    )
+
+
 def create_app(redis_client=None):
+    config = get_config()
+
     app = Flask(__name__)
     CORS(app)
 
     # Maximum number of URLs to receive in an API call
-    app.config['MAXIMUM_POST_URLS'] = 25
+    app.config.update(config)
 
-    embedly_url = 'https://api.embedly.com/1/extract'
-    embedly_key = os.environ.get('EMBEDLY_KEY', None)
+    app.redis_client = redis_client or get_redis_client()
 
-    redis_timeout = 24 * 60 * 60  # 24 hour timeout
-    redis_url = os.environ.get('REDIS_URL', None)
-    app.redis_client = redis_client or redis.StrictRedis(
-        host=redis_url, port=6379, db=0)
-
-    app.extractor = URLExtractor(
-        embedly_url,
-        embedly_key,
-        app.redis_client,
-        redis_timeout,
-    )
+    app.extractor = get_extractor(redis_client)
 
     app.config['VERSION_INFO'] = ''
     if os.path.exists('./version.json'):  # pragma: no cover
@@ -38,14 +59,6 @@ def create_app(redis_client=None):
 
     app.register_blueprint(api.views.blueprint)
 
-    app.config['SENTRY_DSN'] = os.environ.get('SENTRY_DSN', '')
-    app.config['SENTRY_PROCESSORS'] = (
-        'raven.processors.RemovePostDataProcessor',
-    )
     app.sentry = Sentry(app)
-
-    app.config['BLOCKED_DOMAINS'] = [
-        'embedly.com',
-    ]
 
     return app
